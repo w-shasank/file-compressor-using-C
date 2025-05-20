8531c9aee9491d85765bda4e2098e886380302de
Shasank-w <shasankwagle0@gmail.com>
2025-05-20 18:15:09 +0545

    create a headerfile to include all the function prototypes

---
+++ headerfile.h
@@ -0,0 +1,6 @@
+
+
+void write_to_binary_file(const char *text_filename, const char *binary_filename);
+void compress_huffman(const char *binary_filename, const char *compressed_filename);
+void decompress_huffman(const char *compressed_filename);
+void print_file_sizes(const char *file1, const char *file2);
